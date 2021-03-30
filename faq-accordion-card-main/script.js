9174dccd6b97abc31c8888f9c2113df0e5f5cae4
Marcos <marcosdoriguetto@gmail.com>
2021-03-30 16:59:18 -0300

    Implementacao da página faq

---
+++ faq-accordion-card-main/script.js
@@ -0,0 +1,12 @@
+const getQuestions = document.querySelectorAll(".faq--question")
+getQuestions.forEach((question) => question.addEventListener("click", () => {
+
+    if (question.classList.contains("active")) {
+        question.classList.toggle("active")
+    }
+    else {
+        getQuestions.forEach(question => question.classList.remove("active"))
+        question.classList.add("active")
+    }
+
+}))
