099117171a98a864b5690213c7953875036699e0
Marcos <marcosdoriguetto@gmail.com>
2021-03-31 15:22:28 -0300

    Modelo inicial da apresentacao dos sites

---
+++ script.js
@@ -0,0 +1,24 @@
+const doneProjects = ['faq-accordion-card-main', 'ping-coming-soon-page-master', 'profile-card-component-main'];
+
+const listProjects = document.querySelector('ul');
+
+const formatText = text => {
+    return text
+        .split('-')
+        .map(word => word[0].toUpperCase() + word.slice(1))
+        .join(' ');
+}
+
+doneProjects.forEach(name => {
+    const createLi = document.createElement('li');
+
+    createLi.innerHTML = `
+        <a href="/${name}/index.html">
+            <img src="/${name}/design/desktop-design.jpg" alt="${name}" />
+            <p>${formatText(name)}</p>
+        </a>
+    `;
+
+    listProjects.appendChild(createLi);
+})
+
